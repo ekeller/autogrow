bd2ddb743a4db91e1e9446e7d6bdf057790c461c
Erick Keller <erick.keller@gmail.com>
2018-08-05 14:55:31 -0300

    Closes #1

--- autogrow.ino
+++ autogrow.ino
@@ -4,12 +4,30 @@
 /* Thermometer - DS18B20 */
 #include <OneWire.h>
 #include <DallasTemperature.h>
+/* LCD */
+#include <LiquidCrystal.h>
 
-#define TEMPERATURE 22
+#define TEMPERATURE 10
+#define R1 24
+#define R2 25
+#define R3 26
+#define R4 27
+#define R5 28
+#define R6 29
+#define R7 30
+#define R8 31
 
-RTC_DS1307 rtc;
+#define DEBUG false
+
+const int relay_array[] = {R1, R2, R3, R4, R5, R6, R7, R8};
+
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 
 char daysOfTheWeek[7][12] = {"Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"};
+
+LiquidCrystal LCD(rs, en, d4, d5, d6, d7);
+
+RTC_DS1307 rtc;
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(TEMPERATURE);
@@ -17,100 +35,182 @@ OneWire oneWire(TEMPERATURE);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-// arrays to hold device address
 DeviceAddress insideThermometer;
 
-/*
- * printTemperature
- */
-void printTemperature(DeviceAddress deviceAddress) {
-  float tempC = sensors.getTempC(deviceAddress);
-  Serial.print("Temp C: ");
-  Serial.println(tempC);
-  // Serial.print(" Temp F: ");
-  // Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+void discoverOneWireDevices(void) {
+  byte i;
+  byte data[12];
+  byte addr[8];
+  String debugger;
+  
+  while(oneWire.search(addr)) {
+    debugger.concat("Found \'1-Wire\' device with address:\n\r");
+    for(i = 0; i < 8; i++) {
+      debugger.concat("0x");
+      if (addr[i] < 16) {
+        debugger.concat('0');
+      }
+      debugger.concat(String(addr[i], HEX));
+      if (i < 7) {
+        debugger.concat(", ");
+      }
+      insideThermometer[i] = addr[i];
+    }
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+        debugger.concat("CRC is not valid!\n");
+        return;
+    }
+    if (DEBUG) Serial.println(debugger);
+  }
+  oneWire.reset_search();
+  return;
 }
 
-void setup() {
-	Serial.begin(9600);
+/*
+ * Relay setup
+ */
+void relays_setup() {
+  int arraySize = sizeof(relay_array) / sizeof(int);
+  
+  for (int i = 0; i < arraySize; i++) {
+    pinMode(relay_array[i], OUTPUT);
+    
+    if (DEBUG) {
+      Serial.println("SETUP relay on port " + String(relay_array[i-1], DEC));
+    }
+  }
+}
 
-  if (!rtc.begin()) {
+/*
+ * Turns relay on
+ */
+void relays_on(int i) {
+  digitalWrite(relay_array[i-1], HIGH);
+
+  if (DEBUG) {
+    Serial.println("ACTIVATE relay on port " + String(relay_array[i-1], DEC));
+  }
+}
+
+/*
+ * Turns relay off
+ */
+void relays_off(int i) {
+  digitalWrite(relay_array[i-1], LOW);
+
+  if (DEBUG) {
+    Serial.println("DEACTIVATE relay on port " + String(relay_array[i-1], DEC));
+  }
+}
+
+void setup(void) {
+  Serial.begin(9600);
+
+  if (!rtc.begin() && DEBUG) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
 
-  if (!rtc.isrunning()) {
+  if (!rtc.isrunning() && DEBUG) {
     Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
 
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //auto update from computer time
-
-	/*
-	 * Temp sensor
-	 */
-	sensors.begin();
-	Serial.print("Found ");
-	Serial.print(sensors.getDeviceCount(), DEC);
-	Serial.println(" devices.");
-
-	// report parasite power requirements
-	// Serial.print("Parasite power is: ");
-	// if (sensors.isParasitePowerMode()) {
-	// 	Serial.println("ON");
-	// } else {
-	// 	Serial.println("OFF");
-	// }
-
-  // Assign address manually. The addresses below will beed to be changed
-  // to valid device addresses on your bus. Device address can be retrieved
-  // by using either oneWire.search(deviceAddress) or individually via
-  // sensors.getAddress(deviceAddress, index)
-  // Note that you will need to use your specific address here
-  
-  if (!sensors.getAddress(insideThermometer, 0)) {
-    Serial.println("Unable to find address for Device 0");
-  }
-  
-  insideThermometer = { sensors.getAddress(insideThermometer, 0) };
-
-  // show the addresses we found on the bus
-  Serial.print("Device 0 Address: ");
-  printAddress(insideThermometer);
-  Serial.println();
-
-	// set the resolution to 12 bit (Each Dallas/Maxim device is capable of several different resolutions)
-	sensors.setResolution(insideThermometer, 12);
-
-	Serial.print("Device 0 Resolution: ");
-	Serial.print(sensors.getResolution(insideThermometer), DEC);
-	Serial.println();
+  //auto update from computer time when uploading
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // check for oneWire sensors
+  discoverOneWireDevices();
+  // temperature sensor
+  sensors.begin();
+  // setup pinMode for relays
+  relays_setup();
+  // set up the LCD's number of columns and rows
+  LCD.begin(16, 2);
 }
 
-void loop() {
-	DateTime now = rtc.now();
-    
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
+void printTemperature(DeviceAddress deviceAddress){
+  float tempC = sensors.getTempC(deviceAddress);
+  if (tempC == -127.00) {
+    Serial.println("Error getting temperature");
+  } else {
+    Serial.println("Current temperature");
+    Serial.print("C: ");
+    Serial.print(tempC);
+    Serial.print(" F: ");
+    Serial.print(DallasTemperature::toFahrenheit(tempC));
+    Serial.println();
+  }
+}
 
-	sensors.requestTemperatures();
-	printTemperature(insideThermometer);
+void printTemperatureForLCD(DeviceAddress deviceAddress){
+  float tempC = sensors.getTempC(deviceAddress);
+  if (tempC == -127.00) {
+    LCD.clear();
+    LCD.print("Error getting temperature");
+  } else {
+    LCD.print(String("T C:" + String(tempC, 1) + " F:" + String(DallasTemperature::toFahrenheit(tempC), 1)));
+  }
+}
 
+void printDateForLCD(const DateTime now){
+  String dateSeparator = "/";
+  String timeSeparator = ":";
+  String day = now.day() < 9 ? "0" + String(now.day(), DEC) : String(now.day(), DEC);
+  String month = now.month() < 9 ? "0" + String(now.month(), DEC) : String(now.month(), DEC);
+  String year = String(now.year(), DEC).substring(2);
+  
+  String dateTime = day + dateSeparator;
+  dateTime.concat(month + dateSeparator);
+  dateTime.concat(year);
+  dateTime.concat(" ");
+  dateTime.concat(String(now.hour(), DEC) + timeSeparator);
+  dateTime.concat(String(now.minute(), DEC) + timeSeparator);
+  dateTime.concat(String(now.second(), DEC));
+
+  LCD.print(dateTime);
+  
+//  if (dateTime.length() > 16) {
+//    for (int i = 16; i < dateTime.length(); i++) {
+//      LCD.scrollDisplayLeft();
+//    }
+//    delay(1000);
+//    for (int j = 16; j < dateTime.length(); j++) {
+//      LCD.scrollDisplayRight();
+//    }
+//  }
+}
+
+void loop(void) {
+  DateTime now = rtc.now();
+  
+  if (DEBUG) {
+    Serial.println(String(now.year(), DEC) + "/" +
+      String(now.month(), DEC) + "/" +
+      String(now.day(), DEC) + " " +
+      String(now.hour(), DEC) + ":" +
+      String(now.minute(), DEC) + ":" +
+      String(now.second(), DEC)
+    );
+  }
+
+  sensors.requestTemperatures();
+  Serial.print("TEMP: ");
+  printTemperature(insideThermometer);
+
+  LCD.setCursor(0, 0);
+  printDateForLCD(now);
+  LCD.setCursor(0, 1);
+  printTemperatureForLCD(insideThermometer);
+
+  relays_on(1);
+  relays_on(2);
+  relays_on(3);
+  relays_on(4);
+  relays_on(5);
+  relays_on(6);
+  relays_on(7);
+  relays_on(8);
   delay(1000);
 }
